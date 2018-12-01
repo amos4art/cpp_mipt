182675a5415c89fb9f8a622bf6ea344d6e16b351
Alexander Moskalev <amos4art@gmail.com>
2018-11-30 21:42:10 -0500

    added solution for 3 strings

---
+++ white/3_strings/3_strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string min, a, b;
+	cin >> min >> a >> b;
+
+	if (a < min) {
+		min = a;
+	}
+	if (b < min) {
+		min = b;
+	}
+
+	cout << min;
+}
