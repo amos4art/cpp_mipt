decd88dcaa85a97df640c1ff3aa2691e78d52c9f
Alexander Moskalev <amos4art@gmail.com>
2018-11-30 23:14:41 -0500

    fix for equation

--- white/equation/equation.cpp
+++ white/equation/equation.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 
 using namespace std;
 
 int main(){
-	vector<double> res;
-	double a, b, c;
+	double a, b, c, first;
 	cin >> a >> b >> c;
 
-	if (a == 0) {
-		res.push_back(-c / b);
-	}
+	if (a == 0 && b != 0)
+		cout << -c / b;
 	else {
 		double right_part = (b*b) / (4 * a*a) - c / a;
 		if (right_part >= 0) {
 			double right_sqrt = sqrt(right_part);
-			res.push_back(right_sqrt - b / (2 * a));
+			first = right_sqrt - b / (2 * a);
 			double second = -right_sqrt - b / (2 * a);
-			if (second != res[0]) {
-				res.push_back(second);
-			}
+			if (first != second)
+				cout << first << " " << second;
+			else 
+				cout << first;
 		}
 	}
 	
-	for (double x : res) {
-		cout << x;
-	}
-
 	return 0;
 }
