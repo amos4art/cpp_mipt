5f51ff3ecbbfbac2ffd710581474016cba9acb7f
Alexander Moskalev <amos4art@gmail.com>
2018-12-01 00:15:24 -0500

    added solution for 2 number rounded division added solution for discount

--- white/equation/equation.cpp
+++ white/equation/equation.cpp
@@ -7,18 +7,21 @@ int main(){
 	double a, b, c, first;
 	cin >> a >> b >> c;
 
-	if (a == 0 && b != 0)
+	if (a == 0 && b != 0) {
 		cout << -c / b;
+	}
 	else {
 		double right_part = (b*b) / (4 * a*a) - c / a;
 		if (right_part >= 0) {
 			double right_sqrt = sqrt(right_part);
 			first = right_sqrt - b / (2 * a);
 			double second = -right_sqrt - b / (2 * a);
-			if (first != second)
+			if (first != second) {
 				cout << first << " " << second;
-			else 
+			}
+			else {
 				cout << first;
+			}
 		}
 	}
 	
