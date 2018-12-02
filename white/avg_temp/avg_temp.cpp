bafe5d3052750d1aff8e1bc67e5a6033734c3028
Alexander Moskalev <amos4art@gmail.com>
2018-12-02 13:37:40 -0500

    added avg temp

---
+++ white/avg_temp/avg_temp.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static int pow10(int n)
+{
+	static int p10[10] = {
+		1, 10, 100, 1000, 10000,
+		100000, 1000000, 10000000, 100000000, 1000000000
+	};
+
+	return p10[n];
+}
+
+void Reverse(vector<int>& v) {
+	for (int i = 0; i < v.size() / 2; ++i) {
+		int tmp = v[i];
+		v[i] = v[v.size() - 1 - i];
+		v[v.size() - 1 - i] = tmp;
+	}
+}
+
+int main() {
+	int n, avg;
+	int summ = 0;
+	int high = 0;
+	string n_str, s;
+	vector<int> high_vector;
+
+	getline(cin, n_str);
+	n = atoi(n_str.c_str());
+	
+	vector<int> temp(n);
+
+	getline(cin, s);
+	   
+	for (auto& t : temp) {
+		int i = s.size() - 1;
+		int degree = 0;
+		char v;
+		do {
+			v = s[i];
+			if (v != ' ') {
+				t += atoi(&v) * pow10(degree);
+				++degree;
+			}
+			s.erase(i);
+			--i;
+		} while (v != ' ' && i >= 0);
+		summ += t;
+	}
+
+	avg = summ / n;
+
+	Reverse(temp);
+	
+	for (int i = 0; i < temp.size(); ++i) {
+		if (temp[i] > avg) {
+			++high;
+			high_vector.push_back(i);
+		}
+	}
+	
+	cout << high << endl;
+
+	for (auto& h : high_vector) {
+		cout << h << " ";
+	}
+
+	return 0;
+}
