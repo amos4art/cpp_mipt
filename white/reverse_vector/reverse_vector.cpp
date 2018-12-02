3a7f90ca74d9c15216639d827995497c5bca754e
Alexander Moskalev <amos4art@gmail.com>
2018-12-01 23:13:50 -0500

    add solution for move strings and reverse vectors

---
+++ white/reverse_vector/reverse_vector.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+void Reverse(vector<int>& v) {
+	for (int i = 0; i < v.size() / 2; ++i) {
+		int tmp = v[i];
+		v[i] = v[v.size() - 1 - i];
+		v[v.size() - 1 - i] = tmp;
+	}
+}
---
+++ white/reverse_vector_2/reverse_vector_2.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> Reversed(const vector<int>& v) {
+	vector<int> result;
+	for (int i = v.size() - 1; i >= 0 ; --i) {
+		result.push_back(v[i]);
+	}
+	return result;
+}
