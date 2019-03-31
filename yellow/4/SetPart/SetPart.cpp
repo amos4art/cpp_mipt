e570929ff913f99636a2d312f12852c9cf211001
Alexander Moskalev <amos4art@gmail.com>
2019-03-31 00:57:40 -0400

    added set solution

---
+++ yellow/4/SetPart/SetPart.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+	vector<T> res = {};
+	
+	auto it = find_if(begin(elements), end(elements), [&border](const T& i) {return i > border; });
+
+	while (it != end(elements)) {
+		res.push_back(*it);
+		it++;
+	}
+
+	return res;
+}
+
+
+
+int main() {
+	for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {
+		cout << x << " ";
+	}
+	cout << endl;
+
+	string to_find = "Python";
+	cout << FindGreaterElements(set<string>{"C", "C++"}, to_find).size() << endl;
+	return 0;
+}
