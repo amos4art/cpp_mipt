8d45304f8c26eff1b131e6a89bac13c84347e216
Alexander Moskalev <amos4art@gmail.com>
2019-05-10 00:00:01 -0400

    finished week 5 (refactoring task)

--- yellow/5/Refactoring/Personalities.cpp
+++ yellow/5/Refactoring/Personalities.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 class Person {
 public:
-	Person(string name, string prof) : Name(name), Proffession(prof) {
+	Person(const string& name, const string& prof) : Name(name), Proffession(prof) {
 	}
 	
-	virtual void Walk(string& destination) {
+	virtual void Walk(const string& destination) {
 		cout << Proffession << ": " << Name << " walks to: " << destination << endl;
 	}
 
@@ -22,14 +22,14 @@ public:
 class Student : public Person {
 public:
 	
-	Student(string name, string favouriteSong) : Person(name, "Student"), FavouriteSong(favouriteSong) {
+	Student(const string& name, const string& favouriteSong) : Person(name, "Student"), FavouriteSong(favouriteSong) {
 	}
 
 	void Learn() {
 		cout << Proffession << ": " << Name << " learns" << endl;
 	}
 
-	void Walk(string& destination) override {
+	void Walk(const string& destination) override {
 		Person::Walk(destination);
 		SingSong();
 	}
@@ -38,33 +38,31 @@ public:
 		cout << Proffession << ": " << Name << " sings a song: " << FavouriteSong << endl;
 	}
 
-private:
-	string FavouriteSong;
+	const string FavouriteSong;
 };
 
 
 class Teacher : public Person {
 public:
 
-	Teacher(string name, string subject) : Person(name, "Teacher"), Subject(subject) {
+	Teacher(const string& name, const string& subject) : Person(name, "Teacher"), Subject(subject) {
 	}
 
 	void Teach() {
 		cout << Proffession << ": " << Name << " teaches: " << Subject << endl;
 	}
 
-private:
-	string Subject;
+	const string Subject;
 };
 
 
 class Policeman : public Person {
 public:
 	
-	Policeman(string name) : Person(name, "Policeman") {
+	Policeman(const string& name) : Person(name, "Policeman") {
 	}
 
-	void Check(Person& p) {
+	void Check(const Person& p) {
 		cout << "Policeman: " << Name << " checks " << p.Proffession << ". " << 
 			p.Proffession << "'s name is: " << p.Name << endl;
 	}	
