cccd5a7bb98367b57be6e65bac6b6b6bad86773a
Alexander Moskalev <amos4art@gmail.com>
2020-02-26 00:53:58 -0500

    added students and learners solutions

---
+++ red/2/learner/learner.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Learner {
+ private:
+  set<string> dict;
+
+public:
+	int Learn(const vector<string>& words) {
+		int newWords = 0;
+		for (const auto& word : words) {
+			auto ret = dict.insert(word);
+
+			if (ret.second == true) newWords++;
+		}
+		return newWords;
+	}
+
+  vector<string> KnownWords() {
+	  return {dict.begin(), dict.end()};
+  }
+};
---
+++ red/2/students/main.cpp
@@ -0,0 +1,140 @@
+#include "student.h"
+#include "test_runner.h"
+
+#include <algorithm>
+//#include <chrono>
+
+using namespace std;
+//using namespace std::chrono;
+/*
+class LogDuration {
+public:
+	LogDuration() : start(steady_clock::now()) {
+	}
+	~LogDuration() {
+		auto finish = steady_clock::now();
+		auto dur = finish - start;
+		cerr << duration_cast<milliseconds>(dur).count() << "ms" << endl;
+	}
+private:
+	steady_clock::time_point start;
+};
+*/
+
+
+//Оптимизируйте эту функцию
+bool Compare(const Student& first, const Student& second) {
+  return first.Less(second);
+}
+
+void TestComparison() {
+	//LogDuration log;
+	
+	Student newbie {
+    "Ivan", "Ivanov", {
+      {"c++", 1.0},
+      {"algorithms", 3.0}
+    },
+    2.0
+  };
+
+  Student cpp_expert {
+    "Petr", "Petrov", {
+      {"c++", 9.5},
+      {"algorithms", 6.0}
+    },
+    7.75
+  };
+
+  Student guru {
+    "Sidor", "Sidorov", {
+      {"c++", 10.0},
+      {"algorithms", 10.0}
+    },
+    10.0
+  };
+
+  Assert(Compare(guru, newbie), "test1");
+  Assert(Compare(guru, cpp_expert), "test2");
+  Assert(!Compare(newbie, cpp_expert), "test3");
+}
+
+void TestSorting() {
+	//LogDuration log;
+	vector<Student> students {
+    {"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+    {"Semen", "Semenov", {{"maths", 4.}}, 4.},
+    {"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+    {"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+	{"Sidor", "Sidorov", {{"maths", 2.}}, 2.},
+	{"Semen", "Semenov", {{"maths", 4.}}, 4.},
+	{"Ivan", "Ivanov", {{"maths", 5.}}, 5.},
+	{"Petr", "Petrov", {{"maths", 3.}}, 3.},
+    {"Alexander", "Alexandrov", {{"maths", 1.}}, 1.}
+  };
+  sort(students.begin(), students.end(), Compare);
+  Assert(is_sorted(students.begin(), students.end(),
+    [](Student first, Student second) {
+      return first.Less(second);
+    }), "test sorting"
+  );
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestComparison);
+  RUN_TEST(tr, TestSorting);
+  return 0;
+}
